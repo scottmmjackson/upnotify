6165b044d2326acd4e39bf0920eb6d19126aa059
Scott Jackson <scottmmjackson@gmail.com>
2023-08-25 12:08:06 -0400

    ...

--- build/cross-release.sh
+++ build/cross-release.sh
@@ -11,8 +11,29 @@ target=$1
 os=$2
 arch=$3
 
-case $target in
-
+case $os in
+  darwin) case $arch in
+    arm64) cargo_target=aarch64-apple-darwin
+      ;;
+    amd64) cargo_target=x86_64-apple-darwin
+      ;;
+  esac
+  ;;
+  linux) case $arch in
+    arm64)
+      ;;
+    amd64)
+      ;;
+  esac
+  ;;
+  windows)
+    case $arch in
+        arm64)
+          ;;
+        amd64)
+          ;;
+    esac
+    ;;
 esac
 
 # compile x86_64-unknown-linux-gnu linux_amd64
