a306e792417379a67bf10d6a8188d2d9fa8d57eb
Scott Jackson <scottmmjackson@gmail.com>
2023-08-25 11:53:06 -0400

    ...

--- build/cross-release.sh
+++ build/cross-release.sh
@@ -4,7 +4,7 @@ compile() {
   local cargo_target=$1
   local goos=$2
   cargo build --release --target "${cargo_target}" || return
-  cp "target/${cargo_target}/release/upnotify" "dist/${goos}/"
+  cp "target/${cargo_target}/release/upnotify" "dist/upnotify_${goos}/"
 }
 
 # compile x86_64-unknown-linux-gnu linux_amd64
